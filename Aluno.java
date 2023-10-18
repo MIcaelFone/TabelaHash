d0ff26494c9a62fafd9d3618b4590787a6262067
Micael Daniel Pardim Fone <104154840+MicaelDanielPardim@users.noreply.github.com>
2023-10-18 17:23:48 -0300

    Arrumando erro do aluno

--- Aluno.java
+++ Aluno.java
@@ -40,16 +40,4 @@ public class Aluno {
         return aluno_vazio;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Aluno aluno = (Aluno) o;
-        return matricula == aluno.matricula;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(matricula);
-    }
 }
