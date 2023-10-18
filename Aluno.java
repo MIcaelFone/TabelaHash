063145a3d7f0fb8a58691e980f58592aca773076
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2023-10-18 00:09:39 -0300

    Corrigindo Trat. Listas Encadeadas

--- Aluno.java
+++ Aluno.java
@@ -1,38 +1,55 @@
-class Aluno{
-private String nome;
-private int matricula;
+import java.util.Objects;
 
+public class Aluno {
+    private String nome;
+    private int matricula;
 
-public Aluno(){
-    
-    
-}
-public Aluno(int matricula,String nome){
-    this.nome = nome;
-    this.matricula = matricula;
-    
-}
-public String getNome(){
-    return this.nome;
-}
-public void setNome(String nome){
-    this.nome = nome;
-}
-public int getMatricula(){
-    return this.matricula;
-}
-public void setMatricula(int matricula){
-    this.matricula = matricula;
-}
-private static final Aluno aluno_delatado = new Aluno(-2, "Deletado");
-private static final Aluno aluno_vazio = new Aluno(-1, "N/A");
+    public Aluno() {
 
-// Resto da classe Aluno
+    }
 
-public Aluno getAlunoDeletado() {
-    return aluno_delatado;
-}
-public Aluno getAlunoVazio(){
-    return  aluno_vazio;
-}
+    public Aluno(int matricula, String nome) {
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return this.matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    private static final Aluno aluno_deletado = new Aluno(-2, "Deletado");
+    private static final Aluno aluno_vazio = new Aluno(-1, "N/A");
+
+    public static Aluno getAlunoDeletado() {
+        return aluno_deletado;
+    }
+
+    public static Aluno getAlunoVazio() {
+        return aluno_vazio;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return matricula == aluno.matricula;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(matricula);
+    }
 }
--- HashListasEncadeadas.java
+++ HashListasEncadeadas.java
@@ -2,13 +2,12 @@ import java.util.LinkedList;
 import java.util.ArrayList;
 
 public class HashListasEncadeadas {
-    private int maxPosicoes; // Número máximo de posições na tabela hash
-    private int maxItens; // Número máximo de itens que a tabela pode conter
-    private int quantidadeItensAtual; // Número atual de itens na tabela
-    private LinkedList<Aluno>[] estrutura; // Estrutura de armazenamento da tabela hash
-    Aluno aluno = new Aluno(); // Objeto Aluno usado para operações
+    private int maxPosicoes;
+    private int maxItens;
+    private int quantidadeItensAtual;
+    private LinkedList<Aluno>[] estrutura;
+    Aluno aluno = new Aluno();
 
-    // Construtor da classe
     public HashListasEncadeadas(int tamanhoVetor, int maximoItens) {
         this.maxPosicoes = tamanhoVetor;
         this.maxItens = maximoItens;
@@ -17,56 +16,46 @@ public class HashListasEncadeadas {
         inicializarEstrutura(tamanhoVetor);
     }
 
-    // Obtém o número máximo de posições na tabela hash
     public int getMaxPosicoes() {
         return maxPosicoes;
     }
 
-    // Obtém o número máximo de itens que a tabela pode conter
     public int getMaxItens() {
         return maxItens;
     }
 
-    // Obtém o número atual de itens na tabela
     public int getQuantidadeItensAtual() {
         return quantidadeItensAtual;
     }
 
-    // Define o número atual de itens na tabela
     public void setQuantidadeItensAtual(int quantidadeItensAtual) {
         this.quantidadeItensAtual = quantidadeItensAtual;
     }
 
-    // Define o número máximo de posições na tabela hash
     public void setMaxPosicoes(int maxPosicoes) {
         this.maxPosicoes = maxPosicoes;
+        redimensionarTabela(); // Redimensiona a tabela quando o maxPosicoes é alterado
     }
 
-    // Define o número máximo de itens que a tabela pode conter
     public void setMaxItens(int maxItens) {
         this.maxItens = maxItens;
     }
 
-    // Inicializa a estrutura de dados da tabela hash
     public void inicializarEstrutura(int tamanhoVetor) {
         for (int i = 0; i < tamanhoVetor; i++) {
             estrutura[i] = new LinkedList<Aluno>();
         }
     }
 
-    // Calcula a posição na tabela hash com base na matrícula do aluno
     public int funcaoHash(Aluno aluno) {
         return aluno.getMatricula() % maxPosicoes;
     }
 
-    // Insere um aluno na tabela hash
-    // Insere um aluno na tabela hash
     public void Inserir(Aluno aluno) {
         try {
             int local = funcaoHash(aluno);
             LinkedList<Aluno> lista = estrutura[local];
 
-            // Verifica se já existe um aluno com a mesma matrícula
             boolean matriculaExistente = false;
             for (Aluno a : lista) {
                 if (a.getMatricula() == aluno.getMatricula()) {
@@ -93,9 +82,6 @@ public class HashListasEncadeadas {
         }
     }
 
-
-
-    // Redimensiona a tabela hash quando o fator de carga excede um limite
     private void redimensionarTabela() {
         int novoTamanho = maxPosicoes * 2;
         LinkedList<Aluno>[] novaEstrutura = new LinkedList[novoTamanho];
@@ -106,7 +92,7 @@ public class HashListasEncadeadas {
 
         for (int i = 0; i < maxPosicoes; i++) {
             for (Aluno aluno : estrutura[i]) {
-                int local = funcaoHash(aluno);
+                int local = funcaoHash(aluno); // Corrige a função de hash aqui
                 novaEstrutura[local].add(aluno);
             }
         }
@@ -115,54 +101,45 @@ public class HashListasEncadeadas {
         maxPosicoes = novoTamanho;
     }
 
-    // Verifica se a tabela hash está cheia
     public boolean isFull() {
         return quantidadeItensAtual == maxItens;
     }
 
-    // Remove um aluno da tabela hash
     public void Deletar(Aluno aluno) {
         int local = funcaoHash(aluno);
         LinkedList<Aluno> lista = estrutura[local];
 
-        ArrayList<Aluno> alunosRemovidos = new ArrayList<>();
-        lista.removeIf(a -> {
-            if (a.getMatricula() == aluno.getMatricula()) {
-                alunosRemovidos.add(a);
-                return true;
+        boolean alunoEncontrado = false;
+
+        for (Aluno a : lista) {
+            if (a.equals(aluno)) {
+                alunoEncontrado = true;
+                lista.remove(a);
+                quantidadeItensAtual--;
+                System.out.println("O aluno " + a.getNome() + " com a matrícula " + a.getMatricula() + " foi removido!");
+                break;
             }
-            return false;
-        });
+        }
 
-        quantidadeItensAtual -= alunosRemovidos.size();
-
-        if (alunosRemovidos.isEmpty()) {
+        if (!alunoEncontrado) {
             System.out.println("Nenhum aluno encontrado com a matrícula " + aluno.getMatricula());
-        } else {
-            System.out.println("Alunos removidos com a matrícula " + aluno.getMatricula() + ":");
-            for (Aluno removido : alunosRemovidos) {
-                System.out.println("Nome: " + removido.getNome() + ", Matrícula: " + removido.getMatricula());
-            }
         }
     }
 
-    // Busca um aluno na tabela hash
     public void Buscar(Aluno aluno) {
         int local = funcaoHash(aluno);
-        long startTime = System.nanoTime(); // Captura o tempo inicial
+        long startTime = System.nanoTime();
         LinkedList<Aluno> lista = estrutura[local];
         ArrayList<Aluno> alunosEncontrados = new ArrayList<>();
 
         for (Aluno a : lista) {
-            if (a.getMatricula() == aluno.getMatricula()) {
+            if (a.equals(aluno)) {
                 alunosEncontrados.add(a);
             }
         }
 
-        long endTime = System.nanoTime(); // Captura o tempo final
-        long elapsedTime = endTime - startTime; // Calcula o tempo gasto em nanossegundos
-
-        // Converte o tempo para milissegundos
+        long endTime = System.nanoTime();
+        long elapsedTime = endTime - startTime;
         double milliseconds = (double) elapsedTime / 1_000_000;
 
         if (alunosEncontrados.isEmpty()) {
@@ -177,7 +154,6 @@ public class HashListasEncadeadas {
         System.out.println("Tempo gasto na busca: " + milliseconds + " ms");
     }
 
-    // Imprime a tabela hash
     public void Imprimir() {
         System.out.println("Tabela Hash:");
         for (int i = 0; i < maxPosicoes; i++) {
